4c77d3f1120aa1ec672eda25daa9dc4c5c801442
Xin Su <suu@miao.ms>
2026-09-04 23:16:56 +0800

    feat(languages/go): simplify golang config

--- config/languages/go.nix
+++ config/languages/go.nix
@@ -4,57 +4,25 @@
   dependencies.go.enable = false;
 
   extraPackages = with pkgs; [
-    gofumpt
     golangci-lint
     gotools
+    gofumpt
     govulncheck
   ];
 
   plugins.lsp.servers.gopls = {
     enable = true;
+  };
 
-    settings = {
-      gopls = {
-        analyses = {
-          fieldalignment = true;
-          nilness = true;
-          shadow = true;
-          unusedparams = true;
-          unusedwrite = true;
-          useany = true;
-        };
-        codelenses = {
-          gc_details = false;
-          generate = true;
-          regenerate_cgo = true;
-          run_govulncheck = true;
-          test = true;
-          tidy = true;
-          upgrade_dependency = true;
-          vendor = true;
-        };
-        completeUnimported = true;
-        gofumpt = true;
-        hints = {
-          assignVariableTypes = true;
-          compositeLiteralFields = true;
-          compositeLiteralTypes = true;
-          constantValues = true;
-          functionTypeParameters = true;
-          parameterNames = true;
-          rangeVariableTypes = true;
-        };
-        semanticTokens = true;
-        staticcheck = true;
-        usePlaceholders = true;
-      };
-    };
+  plugins.lsp.servers.golangci_lint_ls = {
+    enable = true;
+
+    rootMarkers = [ "go.mod" ];
   };
 
   plugins.conform-nvim.settings.formatters_by_ft.go = [
     "goimports"
     "gofumpt"
+    "golangci-lint"
   ];
-
-  plugins.lint.lintersByFt.go = [ "golangcilint" ];
 }
